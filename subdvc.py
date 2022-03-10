805511bdcdad6059a13b0a57342d64931fe706ff
herboren <herboren@users.noreply.github.com>
2022-03-10 00:34:44 -0500

    Update subdvc.py

--- subdvc.py
+++ subdvc.py
@@ -3,6 +3,37 @@ from urllib import response
 from bs4 import BeautifulSoup
 import requests, re
 
+is_windows = sys.platform.startswith('win')
+
+# Console Colors
+if is_windows:
+    # Windows deserves coloring too :D
+    G = '\033[92m'  # green
+    Y = '\033[93m'  # yellow
+    B = '\033[94m'  # blue
+    R = '\033[91m'  # red
+    W = '\033[0m'   # white
+    try:
+        import win_unicode_console , colorama
+        win_unicode_console.enable()
+        colorama.init()
+        #Now the unicode will work ^_^
+    except:
+        print("[!] Error: Coloring libraries not installed, no coloring will be used [Check the readme]")
+        G = Y = B = R = W = G = Y = B = R = W = ''
+
+
+else:
+    G = '\033[92m'  # green
+    Y = '\033[93m'  # yellow
+    B = '\033[94m'  # blue
+    R = '\033[91m'  # red
+    W = '\033[0m'   # white
+
+def no_color():
+    global G, Y, B, R, W
+    G = Y = B = R = W = ''
+
 # Url
 crt = 'https://crt.sh/?q='
 parameter = ''
