6c6ee164e6909c9cebc1c716c3639d8d2a37b44d
herboren <herboren@users.noreply.github.com>
2022-03-11 00:14:46 -0500

    Update subdvc.py

--- subdvc.py
+++ subdvc.py
@@ -1,7 +1,6 @@
-from posixpath import split
 from urllib import response
 from bs4 import BeautifulSoup
-import requests, re
+import requests, re, os
 from colorama import *
 
 
@@ -10,13 +9,14 @@ Y = Fore.LIGHTYELLOW_EX  # yellow
 B = Fore.LIGHTBLUE_EX  # blue
 R = Fore.LIGHTRED_EX  # red
 W = Fore.LIGHTWHITE_EX  # white
+C = Fore.LIGHTCYAN_EX # Cyan
 
 
-# Url
+# Url used to obtain valid domain certificates
 crt = 'https://crt.sh/?q='
 parameter = ''
-toplevel = ''
-# Header
+
+# Custom Header (https://httpbin.org/get)
 header = {
     "Accept": "*/*",     
     "Accept-Language": "en-US,en;q=0.9", 
@@ -25,46 +25,59 @@ header = {
     "Referer":"https://www.duckduckgo.com"
 }
 
-# Requires http protocol for status
+# Requires status 200
 def GetStatus(status):
     response = requests.get('https://'+status)
     return True if response.status_code == 200 else False
 
+# Get user input
 dirty = input("Input domain name (ex: https://mit.edu): ")
+
+# Need to get second+top level only
 if "/" in dirty:
         clean = re.sub('((https?|ftp|file):\/{2,})|(www\.)','', dirty)        
         parameter = clean.split('/')[0]            
-        print(f"{Y}Working url: {G}{crt}{parameter}")
+        print(f"{G}\nWorking url: {W}{crt}{parameter}")
 
 try:
-    subdomains = []
-    domainlist = []
-    
+    subdomains = [] # Get table rows, listo f list
+
+    # Get page data
     if GetStatus(parameter):
         response = requests.get(crt+parameter, headers=header)
         soup = BeautifulSoup(response.text, 'html.parser')
         trows = soup.find_all('tr')
         
+        # Get 5 element of table data in table row
         for row in trows:
             cols = row.find_all('td')[5:6]
             cols=[x.text for x in cols]
             subdomains.append(cols)   
 
-        rstr = ''
+        rgx_domain = '' # Build a long string
+        dom_nodupe = [] # Holds non-dupe entries
 
-        for row in subdomains: 
-            rstr += str(row)         
-            rstr = re.sub('[^\w+\-\.]', '', rstr)
+        # Regex remove uneccesary SYMBOLS
+        for sd in subdomains:
+            # . and - allowed in subdomain
+            rgx_domain += re.sub('([wW]{3,3}\.)|[\*]\.|([^\w+\-\.])','', str(sd))            
         
-        subdomains = rstr.split('.' + parameter)
-        for row in subdomains: 
-            if len(row) > 1:
-                if row not in domainlist:                
-                    domainlist.append(row)
-        
-        with open(parameter + ".txt", "a", encoding='utf-8') as f:
-            for domain in domainlist:
-                f.write(str(domain) + "\n")
+        # Split subdomains at second+top level
+        rgx_domain = rgx_domain.split(parameter)        
+
+        # Check for empty entries
+        for domain in rgx_domain:
+            if len(str(domain)) != 0:
+                if domain not in dom_nodupe:                    
+                    dom_nodupe.append(domain)
+
+        # Write entries to file
+        if not os.path.isfile(parameter + ".txt"):
+            with open(parameter + ".txt", "a", encoding='utf-8') as f:
+                for d in dom_nodupe:           
+                    f.write(str(d.strip('.')) + "\n")
+                         
+        print(f"{C}Sub-domains written to file: {parameter}.txt{W}")
 
 except Exception as ex:
     print(ex)
